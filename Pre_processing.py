0a26869052015c124ec373438db152b78795ffcd
HNOfficial <109865173+HNOfficial@users.noreply.github.com>
2022-11-10 04:41:30 +0530

    Add files via upload

---
+++ Pre_processing.py
@@ -0,0 +1,43 @@
+import numpy as np
+import glob
+import datetime as dt
+import os
+import pandas as pd
+
+df = pd.read_csv('/Users/harshaannehal/Downloads/Data test/Trip-Info.csv')
+
+df['pd_datetime'] = pd.to_datetime(df['date_time'], format='%Y%m%d%H%M%S')
+
+df['ts'] = df['pd_datetime'].values.astype(np.int64) // 10 ** 9
+
+csv_files = glob.glob('/Users/harshaannehal/Downloads/Data test/EOL-dump/*.csv')
+vehicle_filelist = (pd.read_csv(file) for file in csv_files)
+df_vehlist = pd.concat(vehicle_filelist, ignore_index=True)
+
+reg_number = []
+
+for i in os.listdir('/Users/harshaannehal/Downloads/Data test/EOL-dump/'):
+    i = os.path.splitext(i)[0]
+    reg_number.append(i)
+
+df_filtered = df[df['vehicle_number'].isin(reg_number)]
+
+df_filtered = df_filtered.sort_values(['vehicle_number', 'ts'], ascending=[True, True])
+
+df_vehlist['latlongsum'] = df_vehlist['lat'] + df_vehlist['lon']
+
+df_vehlist_final = df_vehlist.drop_duplicates(['latlongsum'])
+
+start_time_list = df_filtered['ts'].tolist()
+
+df_vehlist_final = df_vehlist_final.drop(columns=['ign', 'case_open', 'panic', 'latlongsum', 'Unnamed: 0'])
+
+df_filtered = df_filtered.drop(columns=['date_time'])
+
+df_vehlist_final = df_vehlist_final.sort_values(['lic_plate_no', 'tis'], ascending=[True, True])
+
+df_filtered.to_csv('trip_info_filtered.csv')
+df_vehlist_final.to_csv('EoL_dump_filtered.csv')
+
+
+
---
+++ main.py
@@ -0,0 +1,52 @@
+from math import radians, cos, sin, asin, sqrt
+import numpy as np
+import glob
+import datetime as dt
+import os
+import streamlit as st
+import pandas as pd
+from report_generator import generator, haversine
+from openpyxl.workbook import Workbook
+
+
+trip_info_filtered = pd.read_csv('/Users/harshaannehal/Downloads/Data test/trip_info_filtered.csv')
+Eol_dump_filtered = pd.read_csv('/Users/harshaannehal/Downloads/Data test/EoL_dump_filtered.csv')
+Eol_dump_filtered = Eol_dump_filtered.sort_values('lic_plate_no')
+trip_info_filtered = trip_info_filtered.sort_values('vehicle_number')
+start_time_list = Eol_dump_filtered['tis'].tolist()
+
+st.markdown('''
+# **Welcome*
+This is the **Report Generator** created in Streamlit using the **pandas** library.
+---
+''')
+
+# Upload CSV data
+with st.sidebar.header('Enter start time and end time in epoch format'):
+    x = st.sidebar.number_input('Start:')
+    y = st.sidebar.number_input('End:')
+    st.sidebar.markdown("""
+Example epoch timestamp:1519842600)
+""")
+
+if st.button('Execute'):
+    @st.experimental_memo
+    def load_data():
+        return generator(x,y)
+
+
+    # Boolean to resize the dataframe, stored as a session state variable
+    st.checkbox("Use container width", value=False, key="use_container_width")
+
+    df = load_data()
+
+    # Display the dataframe and allow the user to stretch the dataframe
+    # across the full width of the container, based on the checkbox value
+    st.dataframe(df)
+    file_path = '/Users/harshaannehal/Downloads/Data test/report.xlsx'
+    df.to_excel('/Users/harshaannehal/Downloads/Data test/report.xlsx')
+    with open(file_path, 'rb') as my_file:
+        st.download_button(label='Download', data=my_file, file_name='filename.xlsx',
+                           mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+st.write('if you do not see any data, kindly re-enter a different set of timestamps')
